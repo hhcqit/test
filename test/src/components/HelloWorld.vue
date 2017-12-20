2405b3420c0b1dd05ec43759397cc3372c67d886
honghuan <hon_ghuan1688@126.com>
2017-12-20 13:05:26 +0800

    Signed-off-by: honghuan <hon_ghuan1688@126.com>

--- test/src/components/HelloWorld.vue
+++ test/src/components/HelloWorld.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
+    <h3>通过计算属性获得store中定义的状态count: {{count}}</h3>
+    <h3>通过计算属性获得store中定义的状态countAlias: {{countAlias}}</h3>
+    <h3>通过计算属性获得store中定义的状态doneTodosCount: {{doneTodosCount}}</h3>
+    <h3>doneTodos: {{doneTodos}}</h3>
+    <h3>
+      this.$store.commit('increment',10);<button @click="increment">count+=10</button>
       <br>
-      <li><a href="http://vuejs-templates.github.io/webpack/" target="_blank">Docs for This Template</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
+      this.$store.commit({type:'incrementObj',amount:10}); <button @click="incrementObj">count+=10</button>
+    </h3>
+    <h3>{{query}}</h3>
+    <!--<h3>-->
+      <!--{{ doneTodos }}-->
+    <!--</h3>-->
     <ul>
       <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
       <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
@@ -21,14 +24,56 @@
 </template>
 
 <script>
+  import {mapState} from 'Vuex'
+  import { mapGetters } from 'vuex'
 export default {
   name: 'HelloWorld',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  computed:
+    mapState({
+    // 箭头函数可使代码更简练
+    count: state => state.count,
+
+    // 传字符串参数 'count' 等同于 `state => state.count`
+    countAlias: 'count',
+
+    // 为了能够使用 `this` 获取局部状态，必须使用常规函数
+    countPlusLocalState (state) {
+      return state.count + this.localCount
+    },
+    doneTodosCount () {
+      return this.$store.getters.doneTodosCount
+    },
+    doneTodos () {
+      return this.$store.getters.doneTodos
+    },
+    query () {
+      return this.$store.getters.getTodoById('school')
+    }
+  }),
+//    {
+//      // 使用对象展开运算符将 getter 混入 computed 对象中
+//      ...mapGetters([
+//        'doneTodosCount',
+//        'doneTodos',
+//        'getTodoById'
+//        // ...
+//      ])
+//    },
+  methods:{
+    increment:function () {
+      this.$store.commit('increment',10);
+    },
+    incrementObj:function () {
+      this.$store.commit({type:'incrementObj',amount:10});
+    }
   }
 }
+
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
